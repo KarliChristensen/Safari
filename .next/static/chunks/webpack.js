f9ff77858eaa9ff6171c82d1ce88cff28284ce4c
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2024-10-21 16:12:26 +0200

    Renamed images for teams functionality

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "59ca7cee2a6a990d"; }
+/******/ 		__webpack_require__.h = function() { return "4aa399333b172cdb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
